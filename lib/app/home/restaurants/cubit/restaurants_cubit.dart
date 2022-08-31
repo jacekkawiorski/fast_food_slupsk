fb5bd84b833825ad3c1752c8b157a9c366c6b845
jacekkawiorski <jkawiorski89@gmail.com>
2022-08-31 17:23:23 +0200

    restaurants cubit

--- lib/app/home/restaurants/cubit/restaurants_cubit.dart
+++ lib/app/home/restaurants/cubit/restaurants_cubit.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:ffi';
+
 import 'package:bloc/bloc.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:meta/meta.dart';
@@ -13,4 +16,45 @@ class RestaurantsCubit extends Cubit<RestaurantsState> {
             isLoading: false,
           ),
         );
+
+  StreamSubscription? _streamSubscription;
+
+  Future<void> start() async {
+    emit(
+      const RestaurantsState(
+        documents: [],
+        errorMessage: '',
+        isLoading: true,
+      ),
+    );
+
+    _streamSubscription = FirebaseFirestore.instance
+        .collection('restaurants')
+        .orderBy('rating', descending: true)
+        .snapshots()
+        .listen((data) {
+      emit(
+        RestaurantsState(
+          documents: data.docs,
+          isLoading: false,
+          errorMessage: '',
+        ),
+      );
+    })
+      ..onError((error) {
+        emit(
+          RestaurantsState(
+            documents: [],
+            isLoading: false,
+            errorMessage: error.toString(),
+          ),
+        );
+      });
+  }
+
+  @override
+  Future<void> close() {
+    _streamSubscription?.cancel();
+    return super.close();
+  }
 }
